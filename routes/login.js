9b7f932761f09aaff4c35652cba0c77c28764ee4
Jmate-2011 <7b7t.net@gmail.com>
2025-05-14 13:14:11 +0000

    nvm

---
+++ routes/login.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const bcrypt = require('bcryptjs');
+
+router.post('/', (req, res) => {
+    const { username, password } = req.body;
+    
+    const usersData = fs.readFileSync(path.join(__dirname, '../users.json'));
+    const users = JSON.parse(usersData);
+
+    const user = users.find(user => user.username === username);
+    
+    if (!user) {
+        res.render('index', { error: 'Username not found' });
+        return;
+    }
+
+    bcrypt.compare(password, user.password, (err, match) => {
+        if (err) {
+            console.error(err);
+            res.render('index', { error: 'An error occurred' });
+        }
+        if (match) {
+            req.session.userId = user.id;
+            req.session.cookie.maxAge = 600000;
+            res.redirect('/home');
+        } else {
+            res.render('index', { error: 'Invalid password' });
+        }
+    });
+});
+
+module.exports = router;
